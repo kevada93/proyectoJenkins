783dc3b0d170db4dadebd66cd0b16a3f88c6e225
ciberseguridad <kevintejeranaranjo@gmail.com>
2023-01-09 19:19:25 +0000

    Primera subida

---
+++ src/main/java/org/example/Miclase.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Miclase {
+    public boolean dia_laboral(String dia) {
+        switch(dia) {
+            case "Lunes":
+                return true;
+            case "Martes":
+                return true;
+            case "Miercoles":
+                return true;
+            case "Jueves":
+                return true;
+            case "Viernes":
+                return true;
+            case "Sabado":
+                return false;
+            case "Domingo":
+                return false;
+            default:
+                return false;
+        }
+    }
+}
